62f0f15d8f2c56cfcf72d7e83951918a8e682cba
yorhodes <yorhodes@cLabs.co>
2021-04-13 15:31:42 -0700

    Fix lint

--- packages/protocol/scripts/check-opcodes.ts
+++ packages/protocol/scripts/check-opcodes.ts
@@ -1,5 +1,5 @@
-import path from 'path'
 import { exec } from 'child_process'
+import path from 'path'
 
 import { CoreContracts } from './build'
 
@@ -10,31 +10,29 @@ const IGNORE_CONTRACTS = ['ReleaseGold', 'TransferWhitelist']
 
 const CHECK_CONTRACTS = CoreContracts.filter((c) => !IGNORE_CONTRACTS.includes(c))
 
-exec(
-  `rg --no-heading "${UNSAFE_OPCODES.join('|')}" ${ROOT_DIR}/contracts`,
-  (err, rg_output, stderr) => {
-    if (err || stderr) {
-      throw new Error('ripgrep failed')
-    }
-
-    const opcode_lines = rg_output.toString().split('\n')
-
-    let safe = true
-    opcode_lines.forEach((line) => {
-      const contract_path = line.split('.sol')[0].split('/')
-      const contract_match = contract_path[contract_path.length - 1]
-      if (CHECK_CONTRACTS.includes(contract_match)) {
-        safe = false
-        console.error(
-          `Core contract ${contract_match} should not include ${UNSAFE_OPCODES} opcodes`
-        )
-      }
-    })
-
-    if (safe) {
-      console.log(`Core contracts are safe against ${UNSAFE_OPCODES} vulnerabilities`)
-    } else {
-      process.exit(1)
-    }
+const handleRgOutput = (err, rgOutput, stderr) => {
+  if (err || stderr) {
+    throw new Error('ripgrep failed')
   }
-)
+
+  const opcodeLines = rgOutput.toString().split('\n')
+
+  let safe = true
+  opcodeLines.forEach((line) => {
+    const contractPath = line.split('.sol')[0].split('/')
+    const contractMatch = contractPath[contractPath.length - 1]
+    if (CHECK_CONTRACTS.includes(contractMatch)) {
+      safe = false
+      console.error(`Core contract ${contractMatch} should not include ${UNSAFE_OPCODES} opcodes`)
+    }
+  })
+
+  if (safe) {
+    // tslint:disable:no-console
+    console.log(`Core contracts are safe against ${UNSAFE_OPCODES} vulnerabilities`)
+  } else {
+    process.exit(1)
+  }
+}
+
+exec(`rg --no-heading "${UNSAFE_OPCODES.join('|')}" ${ROOT_DIR}/contracts`, handleRgOutput)
